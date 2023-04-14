6eb4bda17b6e11760241de798b72c6deb01f4f5e
Vinicius Soares <viniciussoares72943@gmail.com>
2023-04-14 16:08:36 -0300

    feat: implementa funcionalida para buscar noticias pelo titulo

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from tech_news.database import get_collection
+import re
+
+
 # Requisito 7
-def search_by_title(title):
-    """Seu código deve vir aqui"""
+def search_by_title(title: str):
+    result = get_collection().find(
+        {"title": re.compile(title, re.IGNORECASE)},
+        {"_id": False, "title": True, "url": True},
+    )
+
+    return [(item["title"], item["url"]) for item in result]
 
 
 # Requisito 8
