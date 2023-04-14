357c2a0e01b6afa4d7aa881a908676ea00986934
Vinicius Soares <viniciussoares72943@gmail.com>
2023-04-14 19:08:20 -0300

    feat: implementa funcionalidade para retornar as 5 categorias com mais ocorrências

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,21 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10 Vj
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    result = (
+        get_collection()
+        .find(
+            {},
+            {"_id": False, "category": True},
+        )
+        .sort("category", 1)
+    )
+
+    categories_qty = {}
+    for item in result:
+        if item["category"] in categories_qty:
+            categories_qty[item["category"]] += 1
+        else:
+            categories_qty[item["category"]] = 1
+    return sorted(categories_qty, key=categories_qty.get, reverse=True)[:5]
